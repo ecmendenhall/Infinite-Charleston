9feb1f5b155ed08b1bb50a03358655a72d0c78d9
Connor Mendenhall <ecmendenhall@gmail.com>
2012-03-29 16:50:36 +0300

    Fixed video in Mozilla browsers.

--- charleston.js
+++ charleston.js
@@ -29,7 +29,13 @@ function main() {
 	var video_string_mpeg = "http://ecmendenhall.github.com/Infinite-Charleston/media/Trudy" + video_no + ".mp4";
 	var video_string_webm = "http://ecmendenhall.github.com/Infinite-Charleston/media/Trudy" + video_no + ".webm";
 	var videoplayer = document.querySelector(".videoplayer");
-	add_source(videoplayer, video_string_mpeg, "video/mp4");
-	add_source(videoplayer, video_string_mpeg, "video/webm");
+	console.log(navigator.appCodeName);
+	if (navigator.appCodeName === "Mozilla") {
+		add_source(videoplayer, video_string_webm, "video/webm");
+	}
+	else {
+		add_source(videoplayer, video_string_mpeg, "video/mp4");
+		add_source(videoplayer, video_string_mpeg, "video/webm");
+	}
 	get_seconds();
 }
